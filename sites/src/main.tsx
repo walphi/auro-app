2246ad6b27c052c8135d20eea3a8dbb9e99b84e4
User <user@example.com>
2026-01-07 12:57:38 +0400

    Fix MIME type error by removing force redirect and add mounting log

--- sites/src/main.tsx
+++ sites/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('[Agents SPA] Mounting application...');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
