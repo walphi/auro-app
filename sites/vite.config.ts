87717a2b492fb186e973795fb4ce5fc71a9ebf65
User <user@example.com>
2026-01-08 13:46:39 +0400

    fix: configure sites vite build to output to dist/sites

--- sites/vite.config.ts
+++ sites/vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sites/'
+  base: '/sites/',
+  build: {
+    outDir: '../dist/sites',
+    emptyOutDir: true
+  }
 })
